281010f29ef2cd9272fb4065726f8eb5098f4ab4
Jeremy Katras <jakatras@gmail.com>
2016-07-13 13:16:34 -0400

    initial commits
    updated head of index
    **cross origin issue

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(getQuote);
+$('#get-quote-button').on('click', function() {
+  $('#quote-container').fadeOut();
+  getQuote();
+});
+
+function getQuote() {
+  $.ajax({
+    url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    success: function(data) {
+      var post = data.shift();
+      $('#quote').html(post.content);
+      $('#source').text(post.title);
+      displayQuote();
+      var txt = $(post.content).text() + ' -' + post.title;
+      $('#twitter-share').attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent(txt));
+    },
+    cache: false
+  });
+}
+
+function displayQuote() {
+  $('#quote-container').fadeIn();
+}
