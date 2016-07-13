f5b65e537c8e70d41b0aabbc1908ccaa6b570d04
Jeremy Katras <jakatras@gmail.com>
2016-07-13 13:43:49 -0400

    cross origin issue: need to upload to server

--- app.js
+++ app.js
@@ -5,18 +5,29 @@ $('#get-quote-button').on('click', function() {
 });
 
 function getQuote() {
-  $.ajax({
-    url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-    success: function(data) {
+  // $.ajax({
+  //   url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+  //   dataType: "jsonp",
+  //   success: function(data) {
+  //     var post = data.shift();
+  //     $('#quote').html(post.content);
+  //     $('#source').text(post.title);
+  //     displayQuote();
+  //     var txt = $(post.content).text() + ' -' + post.title;
+  //     $('#twitter-share').attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent(txt));
+  //   },
+  //   cache: false
+  // });
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+    function(data) {
       var post = data.shift();
       $('#quote').html(post.content);
       $('#source').text(post.title);
       displayQuote();
       var txt = $(post.content).text() + ' -' + post.title;
       $('#twitter-share').attr("href", "https://twitter.com/intent/tweet?text=" + encodeURIComponent(txt));
-    },
-    cache: false
-  });
+    }
+  );
 }
 
 function displayQuote() {
